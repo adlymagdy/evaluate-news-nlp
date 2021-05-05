7a8e9b848670193fcf4895e9497ed904721ffa88
Adly Magdy <1126032+adlymagdy@users.noreply.github.com>
2021-05-05 05:43:16 +0200

    Added test

---
+++ src/client/__test__/check-input.spec.js
@@ -0,0 +1,14 @@
+import { checkInput } from '../js/check-input';
+
+describe(
+    'input validation function',
+    () => {
+        test(
+            'checkInput should return true if input contains either www or https and otherwise return false',
+            () => {
+                expect(checkInput('www')).toBeTruthy();
+                expect(checkInput('http')).toBeTruthy();
+                expect(checkInput('anything else')).toBeFalsy();
+            }
+            );
+    });
